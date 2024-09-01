7a8736d2023bca260d751fbe07feb86ff3357791
Mathew M <mathewmastromonaco@gmail.com>
2024-09-01 17:59:57 -0400

    front and back end working together.

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 const db = require('./db');
-const dbHelpers = require('./helpers/dbHelpers')(db);
+const dbHelpers = require('./db/helpers/dbHelpers')(db);
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {
     getPostsByUsers
-} = require('../helpers/dataHelpers');
+} = require('../db/helpers/dataHelpers');
 
 module.exports = ({
     getUsers,
