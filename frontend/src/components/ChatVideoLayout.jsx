944d4bd494e3caba989f99c061478336594ccc1b
Sampson Chang <sampson.chang@gmail.com>
2024-09-09 11:23:15 -0700

    small changes

--- frontend/src/components/ChatVideoLayout.jsx
+++ frontend/src/components/ChatVideoLayout.jsx
@@ -8,7 +8,6 @@ const ChatVideo = () => {
   const [playerStats, setPlayerStats] = useState({ player1: {}, player2: {} });
   const [rematch, setRematch] = useState(false);
   const [disconnect, setDisconnect] = useState(false);
-  const [response, setResponse] = useState(false);
 
   const handleGameResult = (result) => {
     setGameResult(result);
@@ -23,9 +22,7 @@ const ChatVideo = () => {
   const handleDisconnect = (boolean) => {
     setDisconnect(boolean);
   };
-  const handleResponse = (boolean) => {
-    setResponse(boolean);
-  };
+
   return (
     <div>
       <h2>Chat Video Layout Componnents</h2>
@@ -37,7 +34,6 @@ const ChatVideo = () => {
         rematch={rematch}
         onDisconnect={handleDisconnect}
         disconnected={disconnect}
-        onResponse={handleResponse}
       />
       {gameResult && (
         <Game
@@ -45,7 +41,6 @@ const ChatVideo = () => {
           onPlayerStatsUpdate={handlePlayerStatsUpdate}
           onDisconnect={handleDisconnect}
           onRematch={handleRematch}
-          response={response}
         />
       )}
     </div>
